d4d9c159b08851f10afe2782aa6642b3968e0d74
Duarms <duarms@yahoo.com>
2022-03-29 02:51:18 +0300

    add pet-form

--- HW2/ui/PetZoo/src/app/app.module.ts
+++ HW2/ui/PetZoo/src/app/app.module.ts
@@ -5,8 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetFormComponent } from './pet-form/pet-form.component';
 
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { HomePageComponent } from './home-page/home-page.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -17,7 +35,12 @@ import { HomePageComponent } from './home-page/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule,
+    BrowserAnimationsModule, MatTableModule, MatPaginatorModule, MatSelectModule, MatInputModule,
+    MatSnackBarModule, FlexLayoutModule, MatListModule, MatDividerModule, MatToolbarModule,
+    MatCardModule, MatIconModule, MatButtonModule,NgxMatFileInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
