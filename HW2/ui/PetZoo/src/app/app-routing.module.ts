f09f702d8306653bb4f28caef1aaee8785958cbb
Duarms <duarms@yahoo.com>
2022-03-29 05:12:41 +0300

    add button

--- HW2/ui/PetZoo/src/app/app-routing.module.ts
+++ HW2/ui/PetZoo/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from "./home-page/home-page.component"
+import {PetFormComponent} from "./pet-form/pet-form.component"
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent
+  },
+  {
+    path: 'new',
+    component: PetFormComponent
   }
 
 ];
